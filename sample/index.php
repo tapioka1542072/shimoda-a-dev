49d1da9b1e130d817160c72812e7f63e22ff1dd9
takanomotochika <s1542064HR@s.chibakoudai.jp>
2017-06-19 17:52:33 +0900

    sample

---
+++ sample/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ろくまる農園</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+		print '<a href="./staff_login/staff_login.html">スタッフ画面</a><br />';
+		print '<a href="./shop/shop_list.php">カスタマ画面</a><br />';
+        ?>
+    </body>
+</html>
