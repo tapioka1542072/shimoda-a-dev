fd85b7560497d77419b4f89fb34bcaabd3b9af3d
tapioka1542072 <shinkurou@icloud.com>
2017-06-20 13:10:09 +0900

    人気商品表示機能

--- sample/shop/shop_list.php
+++ sample/shop/shop_list.php
@@ -24,9 +24,58 @@ else
 <title>ろくまる農園</title>
 </head>
 <body>
-
+    
 <?php
+    try {
 
+//DB接続
+$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+$user='root';
+$password='';
+$dbh=new PDO($dsn,$user,$password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+//商品データ
+$sql='SELECT code,name,price FROM mst_product WHERE 1';
+$stmt1=$dbh->prepare($sql);
+$stmt1->execute();
+//注文データ
+$sql='SELECT code,code_product,quantity FROM dat_sales_product WHERE 1';
+$stmt2=$dbh->prepare($sql);
+$stmt2->execute();
+
+//DB切断
+$dbh=null;
+
+         
+//集計
+while(true)
+{
+	$rec=$stmt2->fetch(PDO::FETCH_ASSOC);
+	if($rec==false)
+	{
+		break;
+	}
+	print $rec['code'];
+	print $rec['code_product'];
+        print $rec['quantity'];
+	print '<br />';
+}
+         
+//ノート
+//表示
+
+
+        
+    } catch (Exception $ex) { print 'ただいま障害により大変ご迷惑をお掛けしております。';
+	 exit();
+        
+    }
+    
+   ?> 
+    
+    
+ <?php   
 try
 {
 
