a5cce83f5a005912c8a583716e97e9e0994fb76d
tapioka1542072 <shinkurou@icloud.com>
2017-06-22 18:58:49 +0900

    n3

--- sample/shop/shop_list.php
+++ sample/shop/shop_list.php
@@ -1,46 +1,47 @@
 <?php
-session_start();
-session_regenerate_id(true);
-if(isset($_SESSION['member_login'])==false)
-{
-	print 'ようこそゲスト様　';
-	print '<a href="member_login.html">会員ログイン</a><br />';
-	print '<br />';
-}
-else
-{
-	print 'ようこそ';
-	print $_SESSION['member_name'];
-	print ' 様　';
-	print '<a href="member_logout.php">ログアウト</a><br />';
-	print '<br />';
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>ろくまる農園</title>
-</head>
-<body>
-    
-<?php
-    
-print ' 人気商品-----------------<br/><br/>';
-
+ session_start();
+ session_regenerate_id(true);
+ if(isset($_SESSION['member_login'])==false)
+ {
+ 	print 'ようこそゲスト様　';
+ 	print '<a href="member_login.html">会員ログイン</a><br />';
+ 	print '<br />';
+ }
+ else
+ {
+ 	print 'ようこそ';
+ 	print $_SESSION['member_name'];
+ 	print ' 様　';
+ 	print '<a href="member_logout.php">ログアウト</a><br />';
+ 	print '<br />';
+ }
+ ?>
+ 
+ <!DOCTYPE html>
+ <html>
+ <head>
+ <meta charset="UTF-8">
+ <title>ろくまる農園</title>
+ </head>
+ <body>
+ 
+ <?php
+       
+print ' 人気商品<br/><br/>';
+ 
 try {
-//DB接続
-$dsn='mysqldbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    
+ //DB接続
+$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+ $user='root';
+ $password='';
+ $dbh=new PDO($dsn,$user,$password);
+ $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 //商品データ
-$sql='SELECT code,name,price FROM mst_product WHERE 1';
-$stmt1=$dbh->prepare($sql);
-$stmt1->execute();
+ $sql='SELECT code,name,price FROM mst_product WHERE 1';
+ $stmt1=$dbh->prepare($sql);
+ $stmt1->execute();
 $p_code=array();
 $p_name=array();
 $p_price=array();
@@ -59,10 +60,10 @@ while(true)
 }
 $pro_num=count($p_code);
 
-//注文データ
-$sql='SELECT code,code_product,quantity FROM dat_sales_product WHERE 1';
-$stmt2=$dbh->prepare($sql);
-$stmt2->execute();
+ //注文データ
+ $sql='SELECT code,code_product,quantity FROM dat_sales_product WHERE 1';
+ $stmt2=$dbh->prepare($sql);
+ $stmt2->execute();
 $s_code=array();
 $s_pro_code=array();
 $s_quantity=array();
@@ -73,25 +74,24 @@ while(true)
  {
  break;
  }
- 
- $s_code[]=$rec['code'];
+  $s_code[]=$rec['code'];
  $s_pro_code[]=$rec['code_product'];
  $s_quantity[]=$rec['quantity'];
 }
 $sales_num=count($s_code);
-
-//DB切断
-$dbh=null;
-
-//集計
+ 
+ //DB切断
+ $dbh=null;
+        
+ //集計
 for ($i = 0; $i < $sales_num; $i++){
  for ($j = 0; $j < $pro_num; $j++){
  if($s_pro_code[$i]===$p_code[$j]){
- $p_sum[$j]=$p_sum[$j]+$s_quantity[$i];
+    $p_sum[$j]=$p_sum[$j]+$s_quantity[$i]; 
  break;
  }
  }
-}
+ }
 
 //ソート
 arsort($p_sum);
@@ -128,67 +128,58 @@ print ' 注文数'.$p_sum[$key].'個';
 print '</a>';
 print '<br />';
 
-    
-        
-  
-
-
-        
-    } catch (Exception $ex) { 
-        print 'ただいま障害により大変ご迷惑をお掛けしております。';
-	 exit();
-        
+} catch (Exception $ex){
+         print 'ただいま障害により大変ご迷惑をお掛けしております。'; 
+         exit(); 
     }
     
-   ?> 
-    
-    
- <?php  
- 
- print '<br />'; 
- print '商品一覧--------------------<br /><br />'; 
- try 
- {
+           ?> 
      
  
-$dsn='mysqldbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-$sql='SELECT code,name,price FROM mst_product WHERE 1';
-$stmt=$dbh->prepare($sql);
-$stmt->execute();
-
-$dbh=null;
-
-print '商品一覧<br /><br />';
-
-while(true)
-{
-	$rec=$stmt->fetch(PDO::FETCH_ASSOC);
-	if($rec==false)
-	{
-		break;
-	}
-	print '<a href="shop_product.php?procode='.$rec['code'].'">';
-	print $rec['name'].'---';
-	print $rec['price'].'円';
-	print '</a>';
-	print '<br />';
-}
-
-print '<br />';
-print '<a href="shop_cartlook.php">カートを見る</a><br />';
-
-}
-catch (Exception $e)
-{
-	 print 'ただいま障害により大変ご迷惑をお掛けしております。';
-	 exit();
-}
-
-?>
-
-</body> 
+ <?php
+ 
+ try
+ {
+ 
+ $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+ $user='root';
+ $password='';
+ $dbh=new PDO($dsn,$user,$password);
+ $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+ 
+ $sql='SELECT code,name,price FROM mst_product WHERE 1';
+ $stmt=$dbh->prepare($sql);
+ $stmt->execute();
+ 
+ $dbh=null;
+ 
+ print '商品一覧<br /><br />';
+ 
+ while(true)
+ {
+ 	$rec=$stmt->fetch(PDO::FETCH_ASSOC);
+ 	if($rec==false)
+ 	{
+ 		break;
+ 	}
+ 	print '<a href="shop_product.php?procode='.$rec['code'].'">';
+ 	print $rec['name'].'---';
+ 	print $rec['price'].'円';
+ 	print '</a>';
+ 	print '<br />';
+ }
+ 
+ print '<br />';
+ print '<a href="shop_cartlook.php">カートを見る</a><br />';
+ 
+ }
+ catch (Exception $e)
+ {
+ 	 print 'ただいま障害により大変ご迷惑をお掛けしております。';
+ 	 exit();
+ }
+ 
+ ?>
+ 
+ </body>
+ </html> 
